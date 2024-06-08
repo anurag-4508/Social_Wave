253f6a5364dd9ada4a7782b0cdb7d9847e3a5ea8
Anurag Mandloi <anuragmandloi30@gmail.com>
2024-06-08 16:10:59 +0530

    commit V1.2

--- social_wave/routes.py
+++ social_wave/routes.py
@@ -216,18 +216,18 @@ def profile(username):
                            , is_following=is_follow)
 
 
-def save_post_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/post_pics', picture_fn)
-
-    output_size = (500, 500)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-
-    return picture_fn
+# def save_post_picture(form_picture):
+#     random_hex = secrets.token_hex(8)
+#     _, f_ext = os.path.splitext(form_picture.filename)
+#     picture_fn = random_hex + f_ext
+#     picture_path = os.path.join(app.root_path, 'static/post_pics', picture_fn)
+#
+#     output_size = (500, 500)
+#     i = Image.open(form_picture)
+#     i.thumbnail(output_size)
+#     i.save(picture_path)
+#
+#     return picture_fn
 
 
 @app.route('/create_post', methods=['POST'])
